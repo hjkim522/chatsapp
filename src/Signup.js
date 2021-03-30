d84d1b07635e630e5482450f5ca312f5881f823d
Hyunjun Kim <njurn@naver.com>
2021-03-30 22:43:54 +0900

    Signup with firebase auth

--- src/Signup.js
+++ src/Signup.js
@@ -1,10 +1,38 @@
+import React, { useReducer, useState } from 'react';
+import { firebaseApp, db, firebase } from './firebase'
+
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const cb = () => {
+    firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+    .then((result) => {
+      //XXX: not transactional
+      console.log(result)
+      db.collection('user')
+      .add({
+        uid: result.user.uid,
+        email: result.user.email,
+        creaated: firebase.firestore.Timestamp.now().seconds
+      })
+      .then((ref) => {
+        setEmail("");
+        setPassword("");
+        //history.push('/signup');
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+      alert(error.message);
+    })
+  }
+
   return (
     <div>
-      <div>email<input></input></div>
-      <div>password<input></input></div>
-      <div>name<input></input></div>
-      <button className="button">Sign Up</button>
+      <div>email<input onChange={(event) => {setEmail(event.target.value)}} value={email}></input></div>
+      <div>password<input onChange={(event) => {setPassword(event.target.value)}} value={password}></input></div>
+      <button className="button" onClick={cb}>Sign Up</button>
     </div>
   );
 }
